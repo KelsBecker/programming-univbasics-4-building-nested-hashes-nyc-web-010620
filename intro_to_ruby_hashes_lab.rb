137ba1078263473e3d5589acbf9d7ef5387dec43
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-16 20:57:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
         :three_pieces_owned => 100,
         :four_pieces_owned => 200},
         :names => {
-          :reading_railroad => {"morgage value" => "$100"},
+          :reading_railroad => 
           :pennsylvania_railroad => 
           :b_and_o_railroad => 
           :shortline => 
@@ -41,8 +41,8 @@ def monopoly_with_fourth_tier
         :three_pieces_owned => 100,
         :four_pieces_owned => 200},
         :names => {
-          :reading_railroad => {"mortgage value"
-          :pennsylvania_railroad =>
+          :reading_railroad => {"mortgage value" => "$100"},
+          :pennsylvania_railroad =>{"mortgage value" => "$200"},
           :b_and_o_railroad =>{"mortgage value" => "$400"},
           :shortline =>{"mortgage value" => "$800"}
         }
