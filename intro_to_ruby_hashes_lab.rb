f4f194b01650ebfc0c6bbd99ba179d317b7b19f0
Kelly Becker <kelly.lynn.becker@gmail.com>
2019-12-16 20:56:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
           :reading_railroad => {"morgage value" => "$100"},
           :pennsylvania_railroad => {"mortgage value" => "$200"},
           :b_and_o_railroad => {"mortgage value" => "$400"},
-          :shortline => {"mortgage value" => "$800"}
+          :shortline => 
         }
   }}
 end
